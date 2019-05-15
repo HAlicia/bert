43323b0842e0662a4970678746f2f8bd541c5c17
huhuhang <huhuhang@gmail.com>
2019-05-15 17:09:40 +0800

    fix

--- cal_accuracy.py
+++ cal_accuracy.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.metrics import accuracy_score
 
-df_test = pd.read_csv("dataset/test.csv")
+df_test = pd.read_csv("test.csv")
 df_pred = pd.read_csv("dataset/output/test_results.tsv", sep='\t', header=-1)
 
 df_pred['label'] = df_pred.idxmax(axis=1)
